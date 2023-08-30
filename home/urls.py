cbb9cb0b54e6a4733c50862c756d45fd1ae71b87
robin kumar shrestha <robin@kasturisanjaal.com>
2023-08-30 23:35:42 +0545

    abc

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 urlpatterns = [
     path("", home, name="home"),
+    path("aboutus", about, name="aboutus"),
+    path("contactus", contact, name="contactus"),
+    path("my_portfolio", portfolio, name="my_portfolio"),
+    path("pricing", price, name="pricing"),
+    path("my_services", services, name="my_services"),
 ]
